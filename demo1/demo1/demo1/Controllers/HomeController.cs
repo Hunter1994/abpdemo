f3ede282366bbb588cf59e660d5cdcbda50ed1a4
zhangdi <di.zhang@ousutec.com.cn>
2020-09-14 17:55:49 +0800

    demo1

---
+++ demo1/demo1/demo1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace demo1.Controllers
+{
+    public class HomeController : AbpController
+    {
+        public string Get()
+        {
+            return "测试";
+        }
+    }
+}
---
+++ demo1/demo1/demo1/demo1Module.cs
@@ -0,0 +1,65 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace demo1
+{
+    [DependsOn(typeof(AbpAutofacModule))]
+    [DependsOn(typeof(AbpAspNetCoreMvcModule))]
+    public class demo1Module:AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            ConfigureSwaggerServices(context.Services);
+        }
+
+        private void ConfigureSwaggerServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(
+               options =>
+               {
+                   options.SwaggerDoc("v1", new OpenApiInfo { Title = "demo API", Version = "v1" });
+                   options.DocInclusionPredicate((docName, description) => true);
+                   options.CustomSchemaIds(type => type.FullName);
+               }
+           );
+
+        }
+
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+     
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "demo API");
+            });
+            app.UseConfiguredEndpoints();
+           
+
+        }
+
+
+
+    }
+}
